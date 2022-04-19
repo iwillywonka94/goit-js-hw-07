2c02f3b6e291fa4e04432e0f2e94238af71f8935
iwillywonka94 <iwillywonka94@gmail.com>
2022-04-19 14:17:28 +0300

    upd 7hw task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery");
+const itemsGallery = galleryItems
+    .map(img => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+    <img
+        class="gallery__image"
+        src="${img.preview}"
+        data-source="${img.original}"
+        alt="${img.description}"
+    />
+    </a>
+    </div>`).join("");
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", itemsGallery)
+gallery.addEventListener("click", onImgClick)
+
+function onImgClick (event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return
+    };
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+    `)
+    instance.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,10 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const gallery = document.querySelector(".gallery")
+const itemsGallery = galleryItems.map(item => 
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+    </a>`
+).join("");
+gallery.insertAdjacentHTML("afterbegin", itemsGallery);
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250, showCounter: false});
